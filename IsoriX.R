5a53ad6b72fac50b7d087fee349f12f28ab5c9fe
vadimukas <vadim.yapiyev@gmail.com>
2019-02-25 19:29:53 +0600

    IsoriX code

---
+++ IsoriX.R
@@ -0,0 +1,10 @@
+# install dependencies
+install.packages("IsoriX", dependencies = TRUE)
+#load Isorix package
+library(IsoriX)
+# online book https://bookdown.org/content/782/
+#set working directory 
+setwd("D:/R/R.code/Isorix")
+# setting global options for isoriX
+options_IsoriX(example_maxtime = Inf)
+options_IsoriX(Ncpu = 4) 
