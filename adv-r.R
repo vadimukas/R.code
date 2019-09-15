746f468e77af2294eeb8675293e250758e85e2c1
vadimukas <vyapiyev@nu.edu.kz>
2019-09-15 21:11:52 +0100

    + a bit for

--- adv-r.R
+++ adv-r.R
@@ -68,4 +68,76 @@ x <- 1:3; names(x) <- c("a", "b", "c")
 x <- setNames(1:3, c("a", "b", "c"))
 # removing names from a vector
 names(x) <- NULL
-#Factor
+#Factors
+# A factor is a vector that can contain only predefined values, and is used to store categorical data.
+
+x <- factor(c("a", "b", "b", "a"))
+x 
+class(x)
+levels(x)
+# You can't use values that are not in the levels
+x[2] <- "c"
+#Factors are useful when you know the possible values a variable may take, even if you don’t see all values
+#in a given dataset. Using a factor instead of a character vector makes it obvious when some groups contain 
+# no observations:
+
+sex_char<-c("m", "m", "m")
+sex_factor<-factor(sex_char, levels = c("m","f"))
+table(sex_char)
+table(sex_factor)
+# Reading in "text" instead of from a file here:
+z <- read.csv(text = "value\n12\n1\n.\n9")
+typeof(z$value)
+as.double
+# Oops, that's not right: 3 2 1 4 are the levels of a factor, 
+# not the values we read in!
+class(z$value)
+as.double<-as.character
+# Or change how we read it in:
+z <- read.csv(text = "value\n12\n1\n.\n9", na.strings=".")
+typeof(z$value)
+#While factors look (and often behave) like character vectors, they are actually integers. 
+#Be careful when treating them like strings. 
+
+#Matrices and arrays
+
+#Matrices and arrays are created with matrix() and array(), or by using the assignment form of dim():
+# Two scalar arguments to specify rows and columns
+a <- matrix(1:6, ncol = 3, nrow = 2)
+# One vector argument to describe all dimensions
+b <- array(1:12, c(2, 3, 2))
+
+# You can also modify an object in place by setting dim()
+c <- 1:6
+dim(c) <- c(3, 2)
+c
+rownames(a) <- c("A", "B")
+colnames(a) <- c("a", "b", "c")
+
+#data frames
+
+df <- data.frame(
+  x = 1:3,
+  y = c("a", "b", "c"),
+  stringsAsFactors = FALSE)
+str(df)
+typeof(df)
+class (df)
+is.data.frame(df)
+# Combining data frames
+# You can combine data frames using cbind() and rbind():
+cbind(df, data.frame(z = 3:1))
+rbind(df, data.frame(x = 10, y = "z"))
+bad <- data.frame(cbind(a = 1:2, b = c("a", "b")))
+str(bad)
+good<-data.frame(cbind(a=1:2, b=c("a","b")),stringsAsFactors = FALSE)
+str(good)
+#Special columns
+df <- data.frame(x = 1:3)
+df$y<-list(1:2,1:3,1:4)
+dfl <- data.frame(x = 1:3, y = I(list(1:2, 1:3, 1:4)))
+str(dfl)
+  
+# Subsetting
+
+  
