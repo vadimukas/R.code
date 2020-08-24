ad3580743e85e5348b225251e6ec7fc7f72da997
vadimukas <vyapiyev@nu.edu.kz>
2020-08-24 09:44:44 +0100

    updating RDS book code with Ch18

--- R for data science/R for data science.R
+++ R for data science/R for data science.R
@@ -1728,3 +1728,94 @@ models %>%
 models %>% 
   map(summary) %>% 
   map_dbl("r.squared")
+
+# 23 Model basics with modeler
+
+# prerequisites
+library(tidyverse)
+library(modelr)
+options(na.action = na.warn)
+
+# simple model
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point() + 
+  theme_bw()
+
+
+models <- tibble(
+  a1 = runif(250, -20, 40),
+  a2 = runif(250, -5, 5)
+)
+
+ggplot(sim1, aes(x, y)) + 
+  geom_abline(aes(intercept = a1, slope = a2), data = models, alpha = 1/4) +
+  geom_point()+
+  theme_test()
+
+#  we first turn our model family into an R function
+model1 <- function(a, data) {
+  a[1] + data$x * a[2]
+}
+model1(c(7, 1.5), sim1)
+
+# next is to compute RMSE
+
+measure_distance <- function(mod, data) {
+  diff <- data$y - model1(mod, data)
+  sqrt(mean(diff ^ 2))
+}
+measure_distance(c(7, 1.5), sim1)
+
+# Now we can use purrr to compute the distance for all the models defined above
+
+sim1_dist <- function(a1, a2) {
+  measure_distance(c(a1, a2), sim1)
+}
+
+models <- models %>% 
+  mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+models
+
+# Next, let’s overlay the 10 best models on to the data.
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point(size = 2, colour = "grey30") + 
+  geom_abline(
+    aes(intercept = a1, slope = a2, colour = -dist), 
+    data = filter(models, rank(dist) <= 10)
+  ) +
+  theme_test()
+
+# We can also think about these models as observations,
+#and visualising with a scatterplot of a1 vs a2, again coloured by -dist
+
+ggplot(models, aes(a1, a2)) +
+  geom_point(data = filter(models, rank(dist) <= 10), size = 4, colour = "red") +
+  geom_point(aes(colour = -dist))+
+  theme_linedraw()
+
+# use grid search to select the best models
+
+grid <- expand.grid(
+  a1 = seq(-5, 20, length = 25),
+  a2 = seq(1, 3, length = 25)
+) %>% 
+  mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+
+grid %>% 
+  ggplot(aes(a1, a2)) +
+  geom_point(data = filter(grid, rank(dist) <= 10), size = 4, colour = "red") +
+  geom_point(aes(colour = -dist)) +
+               theme_test()
+
+# When you overlay the best 10 models back on the original data, they all look pretty good:
+
+ggplot(sim1, aes(x, y)) + 
+  geom_point(size = 2, colour = "grey30") + 
+  geom_abline(
+    aes(intercept = a1, slope = a2, colour = -dist), 
+    data = filter(grid, rank(dist) <= 10)
+  )+
+  theme_light()
+
